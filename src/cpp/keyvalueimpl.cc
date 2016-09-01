a1cff66a6e347ebeb3d343fcacec038d0bb22bef
Charles Cary <cclego@gmail.com>
2016-09-01 08:35:01 -0400

    Revamp model, implement deleteTable and get

--- src/cpp/keyvalueimpl.cc
+++ src/cpp/keyvalueimpl.cc
@@ -1,4 +1,5 @@
 #include "keyvalue.grpc.pb.h"
+#include "keyvalueimpl.h"
 #include "rocksdb/db.h"
 #include "rocksdb/utilities/transaction.h"
 #include "rocksdb/utilities/transaction_db.h"
@@ -10,10 +11,10 @@
 #include <grpc++/server_context.h>
 #include <grpc/grpc.h>
 #include <iostream>
-#include <map>
 #include <mutex>
 #include <shared_mutex>
 #include <string>
+#include <unordered_map>
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -23,97 +24,152 @@ using grpc::Status;
 using keyvalue::KeyValue;
 using rocksdb::TransactionDB;
 
-class KeyValueImpl final : public KeyValue::Service {
-public:
-    explicit KeyValueImpl() {
-        dbDir = "/tmp";         
-        shuffleSource = "0123456789abcdefghijklmnopqrstuvwxyz";
-         
-        // If the tables table doesn't exist, create it.
-        rocksdb::Options options;
-        options.create_if_missing = true;
-        rocksdb::TransactionDBOptions txnOptions;
-        rocksdb::Status status = TransactionDB::Open(options, txnOptions, dbDir + "/tables", &tablesTable);
-        assert(status.ok());
-    }
+const std::string KeyValueImpl::TABLE_ALREADY_EXISTS = "Table already exists";
+const std::string KeyValueImpl::TABLE_DOES_NOT_EXIST = "Table does not exist";
+const std::string KeyValueImpl::DISK_ERROR = "Disk error";
+const std::string KeyValueImpl::INTERNAL_ERROR = "Internal error";
 
-    Status CreateTable(ServerContext* context,
-                       const keyvalue::CreateTableReq* req,
-                       keyvalue::CreateTableRes* res) override {
-        mtx.lock();
-        
-        auto lookup = tableLookup.find(req->name());
-        
-        if (lookup != tableLookup.end()) {
-            //Table already exists
-            mtx.unlock();
-            return Status::OK;
-        }
-        
-        //Table doesn't exist
+KeyValueImpl::KeyValueImpl() {
+    dbDir = "/tmp";         
+    shuffleSource = "0123456789abcdefghijklmnopqrstuvwxyz";
+     
+    // If the tables table doesn't exist, create it.
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::TransactionDBOptions txnOptions;
+    rocksdb::Status status = TransactionDB::Open(options, txnOptions, dbDir + "/tables", &tablesTable);
+    assert(status.ok());
+}
 
-        //Generate the private name of the table
-        std::string tablePrivateName = shuffleSource;
-        std::random_shuffle(tablePrivateName.begin(), tablePrivateName.end()); 
-        std::string tableName = dbDir + "/" + req->name() + "/" + tablePrivateName;
-
-        //Create the table on disk
-        rocksdb::Options options;
-        options.create_if_missing = true;
-        rocksdb::TransactionDBOptions txnOptions;
-        TransactionDB* newTable;
-        rocksdb::Status status = TransactionDB::Open(options, txnOptions, tableName, &newTable);
-        assert(status.ok());
-        
-        //Insert the table into the tables table
-        rocksdb::DB* db = tablesTable->GetBaseDB();
-        db->Put(writeOptions, req->name(), tableName);   
-        
-        // Install into the tableLookup
-        tableLookup[req->name()] = newTable;
-                 
+Status KeyValueImpl::CreateTable(ServerContext* context,
+                                 const keyvalue::CreateTableReq* req,
+                                 keyvalue::CreateTableRes* res) {
+    mtx.lock();
+    
+    auto lookup = tableLookup.find(req->tablename());
+    
+    if (lookup != tableLookup.end()) {
         mtx.unlock();
-        return Status::OK;
-    }
-
-    Status DeleteTable(ServerContext* context,
-                       const keyvalue::DeleteTableReq* req,
-                       keyvalue::DeleteTableRes* res) override {
-        return Status::OK; 
-    }
-
-    Status Get(ServerContext* context,
-               const keyvalue::GetReq* req,
-               keyvalue::GetRes* res) override {
-        return Status::OK;
-    }
-
-    Status Put(ServerContext* context,
-               const keyvalue::PutReq* req,
-               keyvalue::PutRes* res) override {
-        return Status::OK;
-    }
-
-    Status Delete(ServerContext* context,
-                  const keyvalue::DeleteReq* req,
-                  keyvalue::DeleteRes* res) override {
-        return Status::OK;
+        res->set_errorcode(keyvalue::ErrorCode::TABLE_ALREADY_EXISTS);
+        return Status(grpc::StatusCode::ALREADY_EXISTS, TABLE_ALREADY_EXISTS);
     }
     
-    Status Range(ServerContext* context,
-                 const keyvalue::RangeReq* req,
-                 ServerWriter<keyvalue::RangeRes>* writer) override {
-        return Status::OK;
-    }
+    //Table doesn't exist
 
-private:
-    std::shared_timed_mutex mtx;
-    TransactionDB* tablesTable;
-    std::map<std::string, TransactionDB*> tableLookup;
-    std::string dbDir; 
-    std::string shuffleSource;
-    rocksdb::WriteOptions writeOptions;
-};
+    //Generate the private name of the table
+    std::string tablePrivateName = shuffleSource;
+    std::random_shuffle(tablePrivateName.begin(), tablePrivateName.end()); 
+    std::string tableName = dbDir + "/" + req->tablename() + "/" + tablePrivateName;
+
+    //Create the table on disk
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::TransactionDBOptions txnOptions;
+    TransactionDB* newTable;
+    rocksdb::Status status = TransactionDB::Open(options, txnOptions, tableName, &newTable);
+    
+    if (!status.ok()) {
+        //Failed to create table on disk
+        mtx.unlock();
+        res->set_errorcode(keyvalue::ErrorCode::DISK_ERROR); 
+        return Status(grpc::StatusCode::RESOURCE_EXHAUSTED, DISK_ERROR); 
+    }
+    
+    //Insert the table into the tables table
+    rocksdb::DB* db = tablesTable->GetBaseDB();
+    db->Put(writeOptions, req->tablename(), tableName);   
+    
+    // Install into the tableLookup
+    tableLookup[req->tablename()] = newTable;
+             
+    mtx.unlock();
+    res->set_errorcode(keyvalue::ErrorCode::NONE);
+    return Status::OK;
+}
+
+Status KeyValueImpl::DeleteTable(ServerContext* context,
+                                 const keyvalue::DeleteTableReq* req,
+                                 keyvalue::DeleteTableRes* res) {
+    mtx.lock();
+
+    auto lookup = tableLookup.find(req->tablename());
+    
+    if (lookup == tableLookup.end()) {
+        //Table doesn't exist
+        mtx.unlock();
+        res->set_errorcode(keyvalue::ErrorCode::TABLE_DOES_NOT_EXIST);
+        return Status(grpc::StatusCode::NOT_FOUND, TABLE_DOES_NOT_EXIST);
+    }
+    
+    //Table exists
+    rocksdb::TransactionDB* txnDb = lookup->second;
+    
+    tableLookup.erase(req->tablename()); 
+    
+    delete txnDb;
+    
+    //TODO - clean up the on disk files
+     
+    mtx.unlock(); 
+    res->set_errorcode(keyvalue::ErrorCode::NONE);
+    return Status::OK; 
+}
+
+Status KeyValueImpl::Get(ServerContext* context,
+                         const keyvalue::GetReq* req,
+                         keyvalue::GetRes* res) {
+    mtx.lock_shared();
+    
+    auto lookup = tableLookup.find(req->tablename());
+    
+    if (lookup == tableLookup.end()) {
+        //Table doesn't exist
+        mtx.unlock();
+        res->set_errorcode(keyvalue::ErrorCode::TABLE_DOES_NOT_EXIST);
+        return Status(grpc::StatusCode::NOT_FOUND, TABLE_DOES_NOT_EXIST);
+    }
+    
+    //Table exists
+    rocksdb::TransactionDB* txnDb = lookup->second;
+    
+    std::string value; 
+    rocksdb::Status readStatus = txnDb->Get(readOptions, req->key(), &value);
+
+    if (!readStatus.ok()) {
+        //Table doesn't exist
+        mtx.unlock();
+        res->set_errorcode(keyvalue::ErrorCode::INTERNAL_ERROR);
+        return Status(grpc::StatusCode::NOT_FOUND, INTERNAL_ERROR);
+    }
+    
+    keyvalue::Item item;
+    item.set_key(req->key());
+    item.set_value(value);
+        
+    res->mutable_item()->CopyFrom(item);
+      
+    mtx.unlock_shared();
+    res->set_errorcode(keyvalue::ErrorCode::NONE);
+    return Status::OK;
+}
+
+Status KeyValueImpl::Put(ServerContext* context,
+                         const keyvalue::PutReq* req,
+                         keyvalue::PutRes* res) {
+    return Status::OK;
+}
+
+Status KeyValueImpl::Delete(ServerContext* context,
+                            const keyvalue::DeleteReq* req,
+                            keyvalue::DeleteRes* res) {
+    return Status::OK;
+}
+
+Status KeyValueImpl::Range(ServerContext* context,
+                           const keyvalue::RangeReq* req,
+                           ServerWriter<keyvalue::RangeRes>* writer) {
+    return Status::OK;
+}
 
 void RunServer() {
     std::string server_addr("0.0.0.0:8992");
---
+++ src/cpp/keyvalueimpl.h
@@ -0,0 +1,39 @@
+#include "keyvalue.grpc.pb.h"
+#include "rocksdb/db.h"
+#include "rocksdb/utilities/transaction.h"
+#include "rocksdb/utilities/transaction_db.h"
+#include <mutex>
+#include <shared_mutex>
+#include <string>
+#include <unordered_map>
+
+using grpc::ServerContext;
+using grpc::ServerWriter;
+using grpc::Status;
+using keyvalue::KeyValue;
+using rocksdb::TransactionDB;
+
+class KeyValueImpl final : public KeyValue::Service {
+public:
+    explicit KeyValueImpl();
+    Status CreateTable(ServerContext* context, const keyvalue::CreateTableReq* req, keyvalue::CreateTableRes* res) override;
+    Status DeleteTable(ServerContext* context, const keyvalue::DeleteTableReq* req, keyvalue::DeleteTableRes* res) override;
+    Status Get(ServerContext* context, const keyvalue::GetReq* req, keyvalue::GetRes* res) override;
+    Status Put(ServerContext* context, const keyvalue::PutReq* req, keyvalue::PutRes* res) override;
+    Status Delete(ServerContext* context, const keyvalue::DeleteReq* req, keyvalue::DeleteRes* res) override;
+    Status Range(ServerContext* context, const keyvalue::RangeReq* req, ServerWriter<keyvalue::RangeRes>* writer) override;
+
+private:
+    static const std::string TABLE_ALREADY_EXISTS; 
+    static const std::string TABLE_DOES_NOT_EXIST; 
+    static const std::string DISK_ERROR;
+    static const std::string INTERNAL_ERROR;
+    
+    std::shared_timed_mutex mtx;
+    TransactionDB* tablesTable;
+    std::unordered_map<std::string, TransactionDB*> tableLookup;
+    std::string dbDir; 
+    std::string shuffleSource;
+    rocksdb::WriteOptions writeOptions;
+    rocksdb::ReadOptions readOptions;
+};
