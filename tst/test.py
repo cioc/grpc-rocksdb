92f2df9faf7262698386f8afd4916d0b6e50eb8a
Charles Cary <cclego@gmail.com>
2016-09-02 09:45:27 -0400

    passing basic Get and Put tests

--- tst/test.py
+++ tst/test.py
@@ -3,14 +3,25 @@
 import grpc
 import keyvalue_pb2 
 import os
+import sys
 
 if __name__ == '__main__':
     conn_str = os.environ['GRPCROCKSDB_PORT'].split("/")[2]
     print "Connecting on: " + conn_str
     channel = grpc.insecure_channel(conn_str)
-    print "1!"
     stub = keyvalue_pb2.KeyValueStub(channel)
-    print "2!"
     create_table_res = stub.CreateTable(keyvalue_pb2.CreateTableReq(tablename='test-table-1'))
-    print "3!"
-    print create_table_res
+    put_res = stub.Put(keyvalue_pb2.PutReq(tablename='test-table-1',item=keyvalue_pb2.Item(key='myKey', value='12345')))
+    get_res = stub.Get(keyvalue_pb2.GetReq(tablename='test-table-1',key='myKey'))
+    assert get_res.item.value == "12345"
+    try:
+        put_res = stub.Put(keyvalue_pb2.PutReq(tablename='test-table-1',item=keyvalue_pb2.Item(key='myKey', value='99999'),condition="hello"))
+        print "Condition should not be met!"
+        sys.exit(1)
+    except Exception:
+        pass
+    get_res = stub.Get(keyvalue_pb2.GetReq(tablename='test-table-1',key='myKey'))
+    assert get_res.item.value == "12345"
+    put_res = stub.Put(keyvalue_pb2.PutReq(tablename='test-table-1',item=keyvalue_pb2.Item(key='myKey', value='99999'),condition="12345"))
+    get_res = stub.Get(keyvalue_pb2.GetReq(tablename='test-table-1',key='myKey'))
+    assert get_res.item.value == "99999"
