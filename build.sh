914134546c21e47d41824e4903a87caa253bf9d4
Charles Cary <cclego@gmail.com>
2016-09-03 20:42:34 -0400

    Break down build.sh for travis

--- build.sh
+++ build.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
+set -x
 set -e
 
 function kill_container {
-  RUNNING_TEST=$(docker inspect -f {{.State.Running}} $1)
-  if $RUNNING_TEST !== "false"
-  then 
     docker kill $1
-  fi
 }
 
 function delete_container {
-  docker inspect -f {{.State.Running}} $1
-  if [ $? -eq 0 ];
-  then
     docker rm $1
-  fi
 }
 
 function run_build {
@@ -23,42 +16,45 @@ function run_build {
     docker build -t grpc-rocksdb/latest .
 }
 
+function run_build_test {
+    mkdir -p ./tst/protos
+    cp src/protos/* ./tst/protos
+    cd ./tst
+    docker build -t grpc-rocksdb/test .
+}
+
 function run_server {
     docker run -d --expose=8992 -p 8992:8992 -v /tmp --name=grpcrocksdb grpc-rocksdb/latest ./build/grpc-rocksdb 0.0.0.0:8992 /tmp
 }
 
 function run_test {
-    #Build the test container
-    mkdir -p ./tst/protos
-    cp src/protos/* ./tst/protos
-    cd ./tst
-    docker build -t grpc-rocksdb/test .
-
-    #Cleanup anything that may have been left running
-    kill_container grpcrocksdb
-    kill_container grpcrocksdb-test
-    docker rm grpcrocksdb
-    docker rm grpcrocksdb-test
-
     #Run the tests
     echo "TEST START"
     echo "=========="
     echo ""
-    run_server
     docker run -a stdin -a stdout -a stderr -i -t --sig-proxy=true --link grpcrocksdb --name=grpcrocksdb-test grpc-rocksdb/test ./build/test.py
     echo "TEST END"
     echo "========"
     echo ""
-
-    #Cleanup
-    kill_container grpcrocksdb
-    kill_container grpcrocksdb-test
 }
 
 if [ $# -eq 0 ];
 then 
     run_build
+    run_build_test
+    
+    #Cleanup anything that may have been left running
+    kill_container grpcrocksdb || true
+    kill_container grpcrocksdb-test || true
+    docker rm grpcrocksdb || true
+    docker rm grpcrocksdb-test || true
+    
+    run_server
     run_test
+
+    #Cleanup
+    kill_container grpcrocksdb || true
+    kill_container grpcrocksdb-test || true
     exit
 fi
 
@@ -77,4 +73,9 @@ fi
 if [ $1 == "run" ];
 then
     run_server
+fi
+
+if [ $1 == "build-test" ];
+then
+    run_build_test
 fi
